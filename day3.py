603208431bb4cc621f019b31c676dc3f4157bf12
Cedric Maximilian Charles Reuter <cedric.reuter@rwth-aachen.de>
2022-12-17 00:31:39 +0100

    Finished Day3

--- day3.py
+++ day3.py
@@ -13,4 +13,21 @@ def part1():
     return sum(priorities)
 
 
+def part2():
+    data = open("input3.txt").read().splitlines()
+    sum = 0
+    for index in range(0, len(data), 3):
+        first = set(data[index])
+        second = set(data[index + 1])
+        third = set(data[index + 2])
+        item = "".join(first & second & third)
+        if item.islower():
+            sum += ord(item) - 96
+        if item.isupper():
+            sum += ord(item) - 38
+
+    return sum
+
+
 print(part1())
+print(part2())
